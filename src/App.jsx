c9bad1ef295a040bb26f26946203352a607afa82
requ19 <azizmailcom1910@gmail.com>
2024-08-23 19:01:33 +0600

    v4

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,29 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [message, setMessage] = useState('');
-
-  const handleDownload = () => {
-    setMessage('Файл начинает скачиваться...');
-    setTimeout(() => {
-      setMessage(''); // Очистка сообщения через 3 секунды
-    }, 3000);
-  };
-
   return (
+    <>
     <div>
+      <h3>ktiptv-2.0-arm64-v8a</h3>
       <a
-        href="https://disk.yandex.ru/d/VRiqar8z6po4lg"
-        download
+        href="https://disk.yandex.ru/d/DAJBLzyeAgLtsg"
         className="download-btn"
-        onClick={handleDownload}
-        downloand=""
+        target='_blank'
       >
-        Перейти файл
+        Перейти к файлу
       </a>
-      {message && <p>{message}</p>}
     </div>
+    <div>
+      <h3>ktiptv-2.0-v7a</h3>
+      <a
+        href="https://disk.yandex.ru/d/bkHNfWWthwHwPg"
+        className="download-btn"
+        target='_blank'
+      >
+        Перейти к файлу
+      </a>
+    </div>
+    </>
   );
 }
 
