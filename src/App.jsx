78a150e90881067b993e0a75803d5bb6c956c8e1
requ19 <azizmailcom1910@gmail.com>
2024-08-23 22:41:47 +0600

    new button

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,26 @@ function App() {
     <div>
       <h3>ktiptv-2.0-arm64-v8a</h3>
       <a
-        href="https://disk.yandex.ru/d/DAJBLzyeAgLtsg"
+        href="https://online.kt.kg/api/client-update?platform=android&arch=arm64-v8a"
         className="download-btn"
         target='_blank'
       >
-        Перейти к файлу
+        Перейти к загрузке
       </a>
     </div>
     <div>
       <h3>ktiptv-2.0-v7a</h3>
       <a
-        href="https://disk.yandex.ru/d/bkHNfWWthwHwPg"
+        href="https://online.kt.kg/api/client-update?platform=android&arch=armeabi-v7a"
         className="download-btn"
         target='_blank'
       >
-        Перейти к файлу
+        Перейти к загрузке
       </a>
     </div>
     </>
   );
 }
 
-export default DownloadButton;
+export default App;
 
