f9ba2cee3e4e688443c7475572837f3401815082
requ19 <azizmailcom1910@gmail.com>
2024-08-23 19:59:06 +0600

    v10

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,56 @@
-import React, { useState } from 'react';
-import './App.css';
+// import React, { useState } from 'react';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <>
+//     <div>
+//       <h3>ktiptv-2.0-arm64-v8a</h3>
+//       <a
+//         href="https://disk.yandex.ru/d/DAJBLzyeAgLtsg"
+//         className="download-btn"
+//         target='_blank'
+//       >
+//         Перейти к файлу
+//       </a>
+//     </div>
+//     <div>
+//       <h3>ktiptv-2.0-v7a</h3>
+//       <a
+//         href="https://disk.yandex.ru/d/bkHNfWWthwHwPg"
+//         className="download-btn"
+//         target='_blank'
+//       >
+//         Перейти к файлу
+//       </a>
+//     </div>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
+import React from 'react';
+
+function DownloadButton() {
+  const triggerDownload = (fileName) => {
+    const element = document.createElement('a');
+    element.setAttribute('href', fileName);
+    element.setAttribute('download', fileName);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  };
+
   return (
-    <>
-    <div>
-      <h3>ktiptv-2.0-arm64-v8a</h3>
-      <a
-        href="https://disk.yandex.ru/d/DAJBLzyeAgLtsg"
-        className="download-btn"
-        target='_blank'
-      >
-        Перейти к файлу
-      </a>
-    </div>
-    <div>
-      <h3>ktiptv-2.0-v7a</h3>
-      <a
-        href="https://disk.yandex.ru/d/bkHNfWWthwHwPg"
-        className="download-btn"
-        target='_blank'
-      >
-        Перейти к файлу
-      </a>
-    </div>
-    </>
+    <button onClick={() => triggerDownload('../src/apk/ktiptv-2.0-arm64-v8a.apk')}>
+      Скачать файл
+    </button>
   );
 }
 
-export default App;
+export default DownloadButton;
+
