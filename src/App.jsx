d2cf1115eb6e72a9d99154bc03bad50c6f1a489a
requ19 <azizmailcom1910@gmail.com>
2024-08-23 18:32:56 +0600

    IPTV

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  const handleDownload = () => {
+    setMessage('Файл начинает скачиваться...');
+    setTimeout(() => {
+      setMessage(''); // Очистка сообщения через 3 секунды
+    }, 3000);
+  };
+
+  return (
+    <div>
+      <a
+        href="apk/ktiptv-2.0-armeabi-v7a.apk"
+        download
+        className="download-btn"
+        onClick={handleDownload}
+      >
+        Скачать файл
+      </a>
+      {message && <p>{message}</p>}
+    </div>
+  );
+}
+
+export default App;
